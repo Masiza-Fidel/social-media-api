86b800e59891b129cb958feb2f7ff1be0ef54f75
Masiza <fidelmasitsa@gmail.com>
2023-03-12 13:43:29 +0300

    myfirst API

---
+++ Social-Media-App-API-main/README.md
@@ -0,0 +1,42 @@
+# Social-Media-App-API
+A social media API that manages user account creation with access tokens, post creation, update, and deletion, as well as upvotes and downvotes. Built with FastAPI and powered by a Postgres database.
+
+[![Python application](https://github.com/Dev-Elie//Social-Media-App-API/actions/workflows/python-app.yml/badge.svg)](https://github.com/Dev-Elie//Social-Media-App-API/actions/workflows/python-app.yml)
+
+Read the docs [here](https://fastapi-develie.herokuapp.com/docs)
+
+# Installation
+
+1. Navigate into your desired folder, then clone this repo as shown, remember the dot (.) so as to avoid duplicating this repo's name again.
+
+`git clone https://github.com/Dev-Elie/Social-Media-App-API.git .`
+
+2. Change to that specific directory
+
+`cd directory path`
+
+3. Install the requirements from the requirements.txt file.
+
+`pip install -r requirements.txt`
+
+
+4. Create a `.env` file in the root of the directory then add the following contents, adding values for each depending on your configs.
+
+```
+DATABASE_HOSTNAME=
+DATABASE_PORT=
+DATABASE_PASSWORD=
+DATABASE_NAME=
+DATABASE_USERNAME=
+SECRET_KEY=
+ALGORITHM=HS256
+ACCESS_TOKEN_EXPIRE_MINUTES=
+```
+5. Run a database migration
+
+` alembic upgrade head`
+
+6. Start the server
+
+`uvicorn app.main:app --reload`
+
---
+++ Social-Media-App-API-main/app/routers/post.py
@@ -0,0 +1,177 @@
+from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
+from sqlalchemy.orm import Session
+from sqlalchemy import or_, func
+from typing import List, Optional
+from .. import models, schemas, oath2
+from ..database import get_db
+
+router = APIRouter(prefix="/posts", tags=["Posts"])
+
+# Fetch all posts
+
+# @router.get("/all")
+@router.get("/all", response_model=List[schemas.PostOut])
+def get_posts(
+    db: Session = Depends(get_db),
+    limit: int = 10,
+    skip: int = 0,
+    search: Optional[str] = "",
+):
+
+    # posts = (
+    #     db.query(models.Post)
+    #     .filter(
+    #         or_(
+    #             models.Post.title.ilike(f"%{search}%"),
+    #             models.Post.content.ilike(f"%{search}%"),
+    #         )
+    #     )
+    #     .limit(limit)
+    #     .offset(skip)
+    #     .all()
+    # )
+
+    posts = (
+        db.query(models.Post, func.count(models.Vote.post_id).label("votes"))
+        .join(models.Vote, models.Vote.post_id == models.Post.id, isouter=True)
+        .group_by(models.Post.id)
+        .filter(
+            or_(
+                models.Post.title.ilike(f"%{search}%"),
+                models.Post.content.ilike(f"%{search}%"),
+            )
+        )
+        .limit(limit)
+        .offset(skip)
+        .all()
+    )
+
+    return posts
+
+
+# Fetch posts by user id
+@router.get("/users/{id}", response_model=List[schemas.PostOut])
+def get_user_posts(id: int, db: Session = Depends(get_db)):
+
+    posts = db.query(models.Post).filter(models.Post.owner_id == id).all()
+
+    if not posts:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Posts from user with id:{id} not available",
+        )
+
+    return posts
+
+
+# Create a new post
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
+def create_posts(
+    post: schemas.PostCreate,
+    db: Session = Depends(get_db),
+    current_user: int = Depends(oath2.get_current_user),
+):
+
+    new_post = models.Post(owner_id=current_user.id, **post.dict())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+
+    return new_post
+
+
+# Fetch post by post id
+
+
+@router.get("/{id}", response_model=schemas.PostOut)
+def get_post(id: int, db: Session = Depends(get_db)):
+
+    # post = db.query(models.Post).filter(models.Post.id == id).first()
+
+    post = (
+        db.query(models.Post, func.count(models.Vote.post_id).label("votes"))
+        .join(models.Vote, models.Vote.post_id == models.Post.id, isouter=True)
+        .group_by(models.Post.id)
+        .filter(models.Post.id == id)
+        .first()
+    )
+
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"post with id:{id} is not available",
+        )
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"message":f"post with id:{id} is not available"}
+    return post
+
+
+# Delete post
+
+
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(
+    id: int,
+    db: Session = Depends(get_db),
+    current_user: int = Depends(oath2.get_current_user),
+):
+
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+
+    post = post_query.first()
+
+    if post == None:
+
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"post with id:{id} was not found",
+        )
+
+    if post.owner_id != current_user.id:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Not authorized to perform requested action",
+        )
+
+    post_query.delete(synchronize_session=False)
+    db.commit()
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+# Update post
+
+
+@router.put("/{id}", response_model=schemas.Post)
+def update_post(
+    id: int,
+    updated_post: schemas.PostCreate,
+    db: Session = Depends(get_db),
+    current_user: int = Depends(oath2.get_current_user),
+):
+
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+
+    post = post_query.first()
+
+    if post == None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"post with id:{id} was not found",
+        )
+
+    if post.owner_id != current_user.id:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Not authorized to perform requested action",
+        )
+
+    post_query.update(
+        updated_post.dict(),
+        synchronize_session=False,
+    )
+    
+    db.commit()
+    return post_query.first()
